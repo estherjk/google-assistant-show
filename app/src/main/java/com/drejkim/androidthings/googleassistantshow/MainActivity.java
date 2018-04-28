7a25ed21f5302ca00fc9681e1d5db56bc2b9d72b
Esther Jun Kim <esther.jun.kim@gmail.com>
2018-04-28 14:28:20 -0700

    Add Google Assistant implementation

--- app/src/main/java/com/drejkim/androidthings/googleassistantshow/MainActivity.java
+++ app/src/main/java/com/drejkim/androidthings/googleassistantshow/MainActivity.java
@@ -1,7 +1,27 @@
 package com.drejkim.androidthings.googleassistantshow;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.media.AudioDeviceInfo;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.assistant.embedded.v1alpha2.SpeechRecognitionResult;
+import com.google.auth.oauth2.UserCredentials;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Skeleton of an Android Things activity.
@@ -23,10 +43,166 @@ import android.os.Bundle;
  * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
  */
 public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    // Audio constants.
+    private static final String PREF_CURRENT_VOLUME = "current_volume";
+    private static final int SAMPLE_RATE = 16000;
+    private static final int DEFAULT_VOLUME = 100;
+
+    // Assistant SDK constants.
+    private static final String DEVICE_MODEL_ID = "PLACEHOLDER";
+    private static final String DEVICE_INSTANCE_ID = "PLACEHOLDER";
+    private static final String LANGUAGE_CODE = "en-US";
+
+    private Handler mMainHandler;
+    private android.widget.Button mButtonWidget;
+
+    // List & adapter to store and display the history of Assistant Requests.
+    private EmbeddedAssistant mEmbeddedAssistant;
+    private ArrayList<String> mAssistantRequests = new ArrayList<>();
+    private ArrayAdapter<String> mAssistantRequestsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "Starting Google Assistant Show...");
+
         setContentView(R.layout.activity_main);
+        ListView assistantRequestsListView = findViewById(R.id.assistantRequestsListView);
+        mAssistantRequestsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mAssistantRequests);
+        mMainHandler = new Handler(getMainLooper());
+        assistantRequestsListView.setAdapter(mAssistantRequestsAdapter);
+        mButtonWidget = findViewById(R.id.assistantQueryButton);
+        mButtonWidget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mEmbeddedAssistant.startConversation();
+            }
+        });
+
+        // Audio routing configuration
+        AudioDeviceInfo audioInputDevice = null;
+        audioInputDevice = findAudioDevice(AudioManager.GET_DEVICES_INPUTS, AudioDeviceInfo.TYPE_USB_DEVICE);
+        if (audioInputDevice == null) {
+            Log.e(TAG, "Failed to find audio input device...");
+        }
+        AudioDeviceInfo audioOutputDevice = null;
+        audioOutputDevice = findAudioDevice(AudioManager.GET_DEVICES_OUTPUTS, AudioDeviceInfo.TYPE_BUILTIN_SPEAKER);
+        if (audioOutputDevice == null) {
+            Log.e(TAG, "Failed to find audio output device...");
+        }
+
+        // Set volume from preferences
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        int initVolume = preferences.getInt(PREF_CURRENT_VOLUME, DEFAULT_VOLUME);
+        Log.i(TAG, "Setting audio track volume to: " + initVolume);
+
+        // User credentials
+        UserCredentials userCredentials = null;
+        try {
+            userCredentials =
+                    EmbeddedAssistant.generateCredentials(this, R.raw.credentials);
+        } catch (IOException | JSONException e) {
+            Log.e(TAG, "Error getting user credentials...", e);
+        }
+
+        // Assistant
+        mEmbeddedAssistant = new EmbeddedAssistant.Builder()
+                .setCredentials(userCredentials)
+                .setDeviceInstanceId(DEVICE_INSTANCE_ID)
+                .setDeviceModelId(DEVICE_MODEL_ID)
+                .setLanguageCode(LANGUAGE_CODE)
+                .setAudioInputDevice(audioInputDevice)
+                .setAudioOutputDevice(audioOutputDevice)
+                .setAudioSampleRate(SAMPLE_RATE)
+                .setAudioVolume(initVolume)
+                .setDeviceModelId(DEVICE_MODEL_ID)
+                .setDeviceInstanceId(DEVICE_INSTANCE_ID)
+                .setLanguageCode(LANGUAGE_CODE)
+                .setRequestCallback(new EmbeddedAssistant.RequestCallback() {
+                    @Override
+                    public void onRequestStart() {
+                        Log.i(TAG, "Starting assistant request, enable microphones");
+                        mButtonWidget.setText(R.string.button_listening);
+                        mButtonWidget.setEnabled(false);
+                    }
+
+                    @Override
+                    public void onSpeechRecognition(List<SpeechRecognitionResult> results) {
+                        for (final SpeechRecognitionResult result : results) {
+                            Log.i(TAG, "Assistant request text: " + result.getTranscript() +
+                                    "; stability: " + Float.toString(result.getStability()));
+                            mAssistantRequestsAdapter.add(result.getTranscript());
+                        }
+                    }
+                })
+                .setConversationCallback(new EmbeddedAssistant.ConversationCallback() {
+                    @Override
+                    public void onResponseStarted() {
+                        super.onResponseStarted();
+                    }
+
+                    @Override
+                    public void onResponseFinished() {
+                        super.onResponseFinished();
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e(TAG, "Assistant error: " + throwable.getMessage(), throwable);
+                    }
+
+                    @Override
+                    public void onVolumeChanged(int percentage) {
+                        Log.i(TAG, "Assistant volume changed: " + percentage);
+                        // Update our shared preferences
+                        SharedPreferences.Editor editor = PreferenceManager
+                                .getDefaultSharedPreferences(MainActivity.this)
+                                .edit();
+                        editor.putInt(PREF_CURRENT_VOLUME, percentage);
+                        editor.apply();
+                    }
+
+                    @Override
+                    public void onConversationFinished() {
+                        Log.i(TAG, "Assistant conversation finished");
+                        mButtonWidget.setText(R.string.button_new_request);
+                        mButtonWidget.setEnabled(true);
+                    }
+
+                    @Override
+                    public void onAssistantResponse(final String response) {
+                        if(!response.isEmpty()) {
+                            mMainHandler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mAssistantRequestsAdapter.add("Google Assistant: " + response);
+                                }
+                            });
+                        }
+                    }
+                })
+                .build();
+        mEmbeddedAssistant.connect();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "Destroying Google Assistant Show...");
+
+        mEmbeddedAssistant.destroy();
+    }
+
+    private AudioDeviceInfo findAudioDevice(int deviceFlag, int deviceType) {
+        AudioManager manager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+        AudioDeviceInfo[] adis = manager.getDevices(deviceFlag);
+        for (AudioDeviceInfo adi : adis) {
+            if (adi.getType() == deviceType) {
+                return adi;
+            }
+        }
+        return null;
     }
 }
